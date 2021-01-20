b2af0346ea571b7b61f6ab9125f95cad3c671d65
Jihoon Jeong <59548055+jihoojo03@users.noreply.github.com>
2021-01-20 13:31:50 +0900

    Update model.py

--- model.py
+++ model.py
@@ -27,34 +27,6 @@ dilated: change convolution to dilate convolution for fast train if true
 
 def get_unetcbam_model(input_channel_num=3, out_ch=3, start_ch=16, depth=5, inc_rate=2., activation='relu',
          dropout=0.5, ratio=4, batchnorm=True, maxpool=False, upconv=True, residual=False, dilated=False):
-    def _conv_block(m, dim, acti, bn, di, do=0):
-        n = Conv2D(dim, 3, padding='same', dilation_rate=2)(m) if di else Conv2D(dim, 3, padding='same')(m)
-        n = BatchNormalization()(n) if bn else n
-        n = Activation(acti)(n)
-        n = Dropout(do)(n) if do else n
-        n = Conv2D(dim, 3, padding='same', dilation_rate=2)(n) if di else Conv2D(dim, 3, padding='same')(n)
-        n = BatchNormalization()(n) if bn else n
-        n = Activation(acti)(n)
-
-        return n
-
-    def _level_block(m, dim, depth, inc, acti, do, bn, mp, up, di):
-        if depth > 0:
-            n = _conv_block(m, dim, acti, bn, di)
-            m = MaxPooling2D()(n) if mp else Conv2D(dim, 3, strides=2, padding='same')(n)
-            m = _level_block(m, int(inc * dim), depth - 1, inc, acti, do, bn, mp, up, di)
-            if up:
-                m = UpSampling2D()(m)
-                m = Conv2D(dim, 2, activation=acti, padding='same')(m)
-            else:
-                m = Conv2DTranspose(dim, 3, strides=2, activation=acti, padding='same')(m)
-            n = Concatenate()([n, m])
-            m = _conv_block(n, dim, acti, bn, di)
-        else:
-            m = _conv_block(m, dim, acti, bn, di, do)
-
-        return m
-
     def _channel_attention(m, out_ch, ratio):
         avg_pool = GlobalAveragePooling2D()(m)
         avg_pool = Reshape((1,1,out_ch))(avg_pool)
@@ -87,12 +59,40 @@ def get_unetcbam_model(input_channel_num=3, out_ch=3, start_ch=16, depth=5, inc_
         if K.image_data_format() == "channels_first":
             spatial_attention = Permute((3,1,2))(spatial_attention)
 
-        return multiply([m, spatial_attention])
+        return multiply([m, spatial_attention])         
+
+    def _conv_block(m, dim, acti, bn, di, res, do=0):
+        n = Conv2D(dim, 3, padding='same', dilation_rate=2)(m) if di else Conv2D(dim, 3, padding='same')(m)
+        n = BatchNormalization()(n) if bn else n
+        n = Activation(acti)(n)
+        n = Dropout(do)(n) if do else n
+        n = Conv2D(dim, 3, padding='same', dilation_rate=2)(n) if di else Conv2D(dim, 3, padding='same')(n)
+        n = BatchNormalization()(n) if bn else n
+        n = Activation(acti)(n)
+        n = Add()([n, _channel_attention(n, dim)]) if res else _channel_attention(n, dim)
+        n = Add()([n, _spatial_attention(n, dim)]] if res else _spatial_attention(n, dim)
+
+        return n
+
+    def _level_block(m, dim, depth, inc, acti, do, bn, mp, up, res, di):
+        if depth > 0:
+            n = _conv_block(m, dim, acti, bn, di, res)
+            m = MaxPooling2D()(n) if mp else Conv2D(dim, 3, strides=2, padding='same')(n)
+            m = _level_block(m, int(inc * dim), depth - 1, inc, acti, do, bn, mp, up, di)
+            if up:
+                m = UpSampling2D()(m)
+                m = Conv2D(dim, 2, activation=acti, padding='same')(m)
+            else:
+                m = Conv2DTranspose(dim, 3, strides=2, activation=acti, padding='same')(m)
+            n = Concatenate()([n, m])
+            m = _conv_block(n, dim, acti, bn, di, res)
+        else:
+            m = _conv_block(m, dim, acti, bn, di, res, do)
+
+        return m
 
     i = Input(shape=(None, None, input_channel_num))
     o = _level_block(i, start_ch, depth, inc_rate, activation, dropout, batchnorm, maxpool, upconv, residual, dilated)
-    o = Add()([o, _channel_attention(o, start_ch, ratio)]) if residual else _channel_attention(o, start_ch, ratio)
-    o = Add()([o, _spatial_attention(o, start_ch, ratio)]) if residual else _channel_attention(o, start_ch, ratio)
     o = Conv2D(out_ch, 1)(o)
     model = Model(inputs=i, outputs=o)
 
